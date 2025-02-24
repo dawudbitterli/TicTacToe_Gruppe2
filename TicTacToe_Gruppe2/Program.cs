de356965520a74747e735e28c51c632825005a43
David Bitterli <david.bitterli@ksb-sg.ch>
2025-02-24 11:17:40 +0100

    Add project files.

---
+++ TicTacToe_Gruppe2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe_Gruppe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
